a7e42e9e60b37e2af0de898d0d1df9e7c119d6a7
uramonk <kazura1985@gmail.com>
2025-07-05 19:15:54 +0900

    Update HandleExceptions.kt

--- src/main/kotlin/essentials/exceptions/HandleExceptions.kt
+++ src/main/kotlin/essentials/exceptions/HandleExceptions.kt
@@ -1,10 +1,21 @@
 package essentials.exceptions.handleexceptions
 
+import java.lang.ArithmeticException
+import java.lang.NumberFormatException
+
 fun main() {
     while (true) {
         // Wrap below function call with try-catching block,
         // and handle possible exceptions.
-        handleInput()
+        try {
+            handleInput()
+        } catch (e: NumberFormatException) {
+            println("数字を入力してください")
+        } catch (e: IllegalOperatorException) {
+            println("+, -, *, / を入力してください")
+        } catch (e: ArithmeticException) {
+            println("0で割っている")
+        }
     }
 }
 
