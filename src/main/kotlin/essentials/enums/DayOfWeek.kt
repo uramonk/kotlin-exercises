b31b81b8338368acac2c6639af0e69c5b70867f2
uramonk <kazura1985@gmail.com>
2025-07-05 21:34:45 +0900

    Update DayOfWeek.kt

--- src/main/kotlin/essentials/enums/DayOfWeek.kt
+++ src/main/kotlin/essentials/enums/DayOfWeek.kt
@@ -1,12 +1,25 @@
 package essentials.enums.dayofweek
 
-// TODO
+enum class DayOfWeek(val isWeekend: Boolean, val dayName: String) {
+    MONDAY(false, "Monday"),
+    TUESDAY(false, "Tuesday"),
+    WEDNESDAY(false, "Wednesday"),
+    THURSDAY(false, "Thursday"),
+    FRIDAY(false, "Friday"),
+    SATURDAY(true, "Saturday"),
+    SUNDAY(true, "sunday");
+
+    fun nextDay(): DayOfWeek {
+        val nextNum = if (ordinal + 1 == DayOfWeek.entries.size) 0 else ordinal + 1
+        return DayOfWeek.entries[nextNum]
+    }
+}
 
 fun main() {
-    //val friday: DayOfWeek = DayOfWeek.FRIDAY
-    //println(friday.dayName) // Friday
-    //println(friday.isWeekend) // false
-    //val saturday: DayOfWeek = friday.nextDay()
-    //println(saturday.dayName) // Saturday
-    //println(saturday.isWeekend) // true
+    val friday: DayOfWeek = DayOfWeek.FRIDAY
+    println(friday.dayName) // Friday
+    println(friday.isWeekend) // false
+    val saturday: DayOfWeek = friday.nextDay()
+    println(saturday.dayName) // Saturday
+    println(saturday.isWeekend) // true
 }
