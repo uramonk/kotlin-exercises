97a3fa178103b7c9f73940725bc556f2c3633d64
uramonk <kazura1985@gmail.com>
2025-07-05 18:19:21 +0900

    Update Pizza.kt

--- src/main/kotlin/essentials/objects/Pizza.kt
+++ src/main/kotlin/essentials/objects/Pizza.kt
@@ -6,26 +6,34 @@ import kotlin.test.assertEquals
 class Pizza(
     val toppings: List<String>,
 ) {
-    // Class body
+    companion object PizzaFactory {
+        fun hawaiian(): Pizza {
+            return Pizza(listOf("ham", "pineapple"))
+        }
+
+        fun margherita(): Pizza {
+            return Pizza(listOf("tomato", "mozzarella"))
+        }
+    }
 }
 
-fun main() {
-    //val hawaiian = Pizza.hawaiian()
-    //println(hawaiian.toppings) // [ham, pineapple]
-    //val margherita = Pizza.margherita()
-    //println(margherita.toppings) // [tomato, mozzarella]
-}
+//fun main() {
+//val hawaiian = Pizza.hawaiian()
+//println(hawaiian.toppings) // [ham, pineapple]
+//val margherita = Pizza.margherita()
+//println(margherita.toppings) // [tomato, mozzarella]
+//}
 
 class PizzaTest {
-//    @Test
-//    fun testHawaiian() {
-//        val expected = listOf("ham", "pineapple")
-//        assertEquals(expected, Pizza.hawaiian().toppings)
-//    }
-//
-//    @Test
-//    fun testMargherita() {
-//        val expected = listOf("tomato", "mozzarella")
-//        assertEquals(expected, Pizza.margherita().toppings)
-//    }
+    @Test
+    fun testHawaiian() {
+        val expected = listOf("ham", "pineapple")
+        assertEquals(expected, Pizza.hawaiian().toppings)
+    }
+
+    @Test
+    fun testMargherita() {
+        val expected = listOf("tomato", "mozzarella")
+        assertEquals(expected, Pizza.margherita().toppings)
+    }
 }
