c86d1d572b8a262bb860f4d49c0b9c44787dc1fc
uramonk <kazura1985@gmail.com>
2025-07-10 07:53:41 +0900

    Update TopArticles.kt

--- src/main/kotlin/functional/collections/TopArticles.kt
+++ src/main/kotlin/functional/collections/TopArticles.kt
@@ -6,7 +6,12 @@ import org.junit.Test
 class TopArticlesGenerator(
     private val articles: List<ArticleStatistics>,
 ) {
-    fun topArticles(n: Int): List<ArticleStatistics> = TODO()
+    fun topArticles(n: Int): List<ArticleStatistics> = articles
+        .withIndex()
+        .sortedByDescending { it.value.views }
+        .take(n)
+        .sortedBy { it.index }
+        .map { it.value }
 }
 
 data class ArticleStatistics(
@@ -14,23 +19,23 @@ data class ArticleStatistics(
     val views: Long,
 )
 
-fun main() {
-    val generator = TopArticlesGenerator(
-        listOf(
-            ArticleStatistics("Article 1", 400),
-            ArticleStatistics("Article 2", 100),
-            ArticleStatistics("Article 3", 200),
-            ArticleStatistics("Article 4", 300),
-            ArticleStatistics("Article 5", 500),
-            ArticleStatistics("Article 6", 0),
-        )
-    )
-    val topArticles = generator.topArticles(3)
-    topArticles.onEach { println(it) }
-    // ArticleStatistics(title=Article 1, views=400)
-    // ArticleStatistics(title=Article 4, views=300)
-    // ArticleStatistics(title=Article 5, views=500)
-}
+//fun main() {
+//    val generator = TopArticlesGenerator(
+//        listOf(
+//            ArticleStatistics("Article 1", 400),
+//            ArticleStatistics("Article 2", 100),
+//            ArticleStatistics("Article 3", 200),
+//            ArticleStatistics("Article 4", 300),
+//            ArticleStatistics("Article 5", 500),
+//            ArticleStatistics("Article 6", 0),
+//        )
+//    )
+//    val topArticles = generator.topArticles(3)
+//    topArticles.onEach { println(it) }
+//    // ArticleStatistics(title=Article 1, views=400)
+//    // ArticleStatistics(title=Article 4, views=300)
+//    // ArticleStatistics(title=Article 5, views=500)
+//}
 
 class TopArticlesTest {
     @Test
@@ -43,7 +48,7 @@ class TopArticlesTest {
             ArticleStatistics("Article 5", 500),
             ArticleStatistics("Article 6", 0),
 
-        )
+            )
         val generator = TopArticlesGenerator(articles)
         val topArticles = generator.topArticles(100)
         assertEquals(articles, topArticles)
@@ -59,7 +64,7 @@ class TopArticlesTest {
             ArticleStatistics("Article 5", 500),
             ArticleStatistics("Article 6", 0),
 
-        )
+            )
         val generator = TopArticlesGenerator(articles)
         val topArticles = generator.topArticles(3)
         assertEquals(articles.slice(listOf(0, 3, 4)), topArticles)
