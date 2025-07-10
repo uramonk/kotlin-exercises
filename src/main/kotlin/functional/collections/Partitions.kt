903433fdf69d9c2b7036bee080c99022e437b992
uramonk <kazura1985@gmail.com>
2025-07-11 07:52:54 +0900

    Update Partitions.kt

--- src/main/kotlin/functional/collections/Partitions.kt
+++ src/main/kotlin/functional/collections/Partitions.kt
@@ -3,7 +3,26 @@ package functional.collections.partitions
 import org.junit.Test
 import kotlin.test.assertEquals
 
-fun <T> Collection<T>.partitions(): Set<Set<Set<T>>> = TODO()
+fun <T> Collection<T>.partitions(): Set<Set<Set<T>>> {
+    if (this.size <= 1) return setOf(setOf(this.toSet()))
+    val n = this.first()
+    val list = this.drop(1)
+    val set = list.partitions()
+
+    val whereHeadIsAlone: Set<Set<Set<T>>> = set
+        .map { it + setOf(setOf(n)) }
+        .toSet()
+    val whereHeadIsNotAlone: Set<Set<Set<T>>> = set
+        .flatMap { partition: Set<Set<T>> ->
+            partition.map { subset: Set<T> ->
+                partition
+                    .minusElement(subset)
+                    .plusElement(subset + n)
+            }
+        }
+        .toSet()
+    return whereHeadIsAlone + whereHeadIsNotAlone
+}
 
 class PartitionsTest {
     @Test
