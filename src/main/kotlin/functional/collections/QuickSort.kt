fab75225d7bf4fae7095ca2df50d3cabf98dceed
uramonk <kazura1985@gmail.com>
2025-07-11 07:31:01 +0900

    Update QuickSort.kt

--- src/main/kotlin/functional/collections/QuickSort.kt
+++ src/main/kotlin/functional/collections/QuickSort.kt
@@ -7,7 +7,12 @@ import kotlin.test.assertEquals
 // TODO: Quick sort should take first element (pivot), then split rest to bigger than pivot and smaller and finally return
 //  first smaller sorted, then pivot and finally bigger sorted
 fun <T : Comparable<T>> List<T>.quickSort(): List<T> {
-    TODO()
+    if (this.size <= 1) {
+        return this
+    }
+    val result = this.drop(1).partition { it <= this.first() }
+    return result.first.quickSort() + this.first() + result.second.quickSort()
+
 }
 
 class QuickSortTest {
